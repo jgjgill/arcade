dd27d946a9abc41d0f65cc6972cb217510242d04
jgjgill <79239852+jgjgill@users.noreply.github.com>
2022-11-20 19:55:06 +0900

    first commit Vite, eslint, stylelint, prettier 설정

---
+++ README.md
@@ -0,0 +1,1 @@
+# jgjgill-study
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@src', replacement: resolve(__dirname, 'src') },
+      { find: '@assets', replacement: resolve(__dirname, 'src/assets') },
+      { find: '@components', replacement: resolve(__dirname, 'src/components') },
+      { find: '@styles', replacement: resolve(__dirname, 'src/styles') },
+    ],
+  },
+})
