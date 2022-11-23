0f9b6d0856c4fbf86840e111fcfaa485dfbd67ad
jgjgill <79239852+jgjgill@users.noreply.github.com>
2022-11-23 18:08:49 +0900

    chore: 배포 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  base: 'arcade',
   resolve: {
     alias: [
       { find: '@src', replacement: resolve(__dirname, 'src') },
