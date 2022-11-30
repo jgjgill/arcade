b86de351af9557036de94840d7f51546a266ba73
jgjgill <79239852+jgjgill@users.noreply.github.com>
2022-11-30 17:34:09 +0900

    feat: 랜덤 지뢰 생성 처음 클릭 인덱스 제외 함수 행, 열 기준 인덱스 계산 함수 댄럼 지뢰 개수 계산 함수

--- src/pages/minesweeper/index.tsx
+++ src/pages/minesweeper/index.tsx
@@ -1,9 +1,16 @@
 import { Board } from '@minesweeper/components'
 import styles from '@minesweeper/minesweeper.module.scss'
+import { createMineArray } from '@src/utils/minesweeper'
 
 const Minesweeper = () => {
+  const temp = createMineArray({ clickIndex: 10, length: 100, mineCount: 10 })
+
+  console.log(temp)
   return (
     <div className={styles.container}>
+      <div>
+        <div>123</div>
+      </div>
       <Board />
     </div>
   )
--- src/utils/minesweeper.ts
+++ src/utils/minesweeper.ts
@@ -6,4 +6,44 @@ export const buildBoard = ({ row, column }: { row: number; column: number }) => 
   return board
 }
 
+interface Temp {
+  length: number
+  clickIndex: number
+  mineCount: number
+}
+
+interface Calc {
+  row: number
+  clickRow: number
+  clickColumn: number
+}
+
+export const calcArrayIndex = ({ row, clickRow, clickColumn }: Calc) => {
+  return row * clickRow + clickColumn
+}
+
+export const createNotClickIndexArray = (length: number, clickIndex: number) => {
+  return Array.from({ length }, (_, i) => i).filter((item) => item !== clickIndex)
+}
+
+export const createMineArray = ({ length, clickIndex, mineCount }: Temp) => {
+  const numbers = createNotClickIndexArray(length, clickIndex)
+
+  const mineArray = []
+
+  while (mineArray.length !== mineCount) {
+    const index = Math.floor(Math.random() * numbers.length)
+    const random = numbers.splice(index, 1)[0]
+
+    mineArray.push(random)
+  }
+
+  console.log(numbers)
+  return mineArray
+}
+
 // 랜덤 폭탄 요소
+// 필요한 데이터 => 행, 열, 지뢰 개수, 클릭한 인덱스
+
+// 배열 크기 숫자에서 클릭 인덱스 삭제
+// 지뢰 개수
